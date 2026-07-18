1779e4ebf5717680319c468624a14cb360a1e2de
JoseZenteno269 <zentenojose123456@gmail.com>
2026-07-18 00:33:23 -0300

    Nuevo Diseño y Mejoras

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -16,3 +16,18 @@ proyecto1.addEventListener("click", () => {
     window.location.href = "https://github.com/JoseZenteno269/PROYECTO-CLINICA";
     
 });
+
+const proyecto2 = document.getElementById("btn_proyecto2"); 
+
+proyecto2.addEventListener("click", () => {
+    window.location.href = "https://github.com/JoseZenteno269/SISTEMA-DE-GESTION-TRASNPORTE";
+});
+
+const links = document.querySelectorAll(".menu_lateral a");
+const menulateral = document.querySelector(".menu_lateral");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        menulateral.classList.remove("abierto");
+    });
+});
